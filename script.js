8a5977e448a4e13816161cb3e7bab75963531941
Kurrem <oscar.harmsen@gmail.com>
2024-09-25 14:53:42 +0200

    Extra functionality - square color is now randomised and opacity is incremented by 10% for each color change (starts at 0%)

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 let container = document.querySelector(".container")
 let boardWidth = 16
 
-newBoardButton = document.querySelector("#newBoard")
-newBoardButton.addEventListener("click", () => {
-    boardWidth = prompt("Please specify the amount of squares for each side, max 100.")
-    if (boardWidth > 100) boardWidth = 100
-    createBoard(boardWidth)
-})
+function squareColor() {
+    hue = Math.random()*360
+    saturation = Math.random()*100
+    lightness = Math.random()*100
+    hslVar = "hsl(" + hue + " " + saturation + " " + lightness + ")"
+
+    return hslVar
+}
+
+function squareOpacity (square) {
+    opacity = square.style.opacity
+    if (!opacity) {
+        opacity = 0.1
+    } else {
+        opacity = parseFloat(opacity) + 0.1
+    }
+    return opacity
+}
 
 function createBoard(boardWidth) {
     container.innerHTML = ""
@@ -17,7 +29,10 @@ function createBoard(boardWidth) {
             let square = document.createElement("div");
             square.classList.add("square")
     
-            square.addEventListener("mouseover", () => {square.style.background = ("black")})
+            square.addEventListener("mouseover", () => {
+                square.style.opacity = (squareOpacity(square))
+                square.style.background = (squareColor())
+            })
     
             col.appendChild(square)
         }
@@ -25,5 +40,12 @@ function createBoard(boardWidth) {
         container.appendChild(col)
     }
 }
+
+newBoardButton = document.querySelector("#newBoard")
+newBoardButton.addEventListener("click", () => {
+    boardWidth = prompt("Please specify the amount of squares for each side, max 100.")
+    if (boardWidth > 100) boardWidth = 100
+    createBoard(boardWidth)
+})
 
 createBoard(boardWidth)
