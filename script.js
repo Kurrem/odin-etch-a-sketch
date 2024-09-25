7684bb034a584e2baa3911c9d7da93744498014a
Kurrem <oscar.harmsen@gmail.com>
2024-09-25 09:01:30 +0200

    Added functionality to create a new board with a desired width/height.

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 let container = document.querySelector(".container")
+let boardWidth = 16
 
-for (let i = 0; i < 16; i++) {
-    let col = document.createElement("div");
-    col.classList.add("col");
-    for (let j = 0; j < 16; j++) {
-        let square = document.createElement("div");
-        square.classList.add("square")
+newBoardButton = document.querySelector("#newBoard")
+newBoardButton.addEventListener("click", () => {
+    boardWidth = prompt("Please specify the amount of squares for each side, max 100.")
+    if (boardWidth > 100) boardWidth = 100
+    createBoard(boardWidth)
+})
 
-        square.addEventListener("mouseover", () => {square.style.background = ("black")})
-
-        col.appendChild(square)
+function createBoard(boardWidth) {
+    container.innerHTML = ""
+    for (let i = 0; i < boardWidth; i++) {
+        let col = document.createElement("div");
+        col.classList.add("col");
+        for (let j = 0; j < boardWidth; j++) {
+            let square = document.createElement("div");
+            square.classList.add("square")
+    
+            square.addEventListener("mouseover", () => {square.style.background = ("black")})
+    
+            col.appendChild(square)
+        }
+        console.log(container)
+        container.appendChild(col)
     }
-    console.log(container)
-    container.appendChild(col)
 }
+
+createBoard(boardWidth)
