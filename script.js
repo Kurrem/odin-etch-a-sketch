7576674903d7868dd19d56c74409629846939f5d
Kurrem <oscar.harmsen@gmail.com>
2024-09-25 08:14:58 +0200

    Added eventlistener for squares to turn black on mouseover event. Making them "drawable" as an etch-a-sketch.

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         let square = document.createElement("div");
         square.classList.add("square")
+
+        square.addEventListener("mouseover", () => {square.style.background = ("black")})
+
         col.appendChild(square)
     }
     console.log(container)
